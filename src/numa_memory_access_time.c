3f7942c430a31a5824d2f8775c04026514d62171
sudormroot <sudormroot@163.com>
2019-08-03 10:22:13 +0800

    modify numa_memory_access_time.c

--- src/numa_memory_access_time.c
+++ src/numa_memory_access_time.c
@@ -64,19 +64,39 @@ inline uint64_t rdtsc(void)
 
 double measure_cpu_freq(void)
 {
-	uint64_t t1,t2;
+	uint64_t c1,c2;
 	double freq;
-	int interval = 5;
+	int interval = 2;
+
+	struct timeval t1, t2;
+
+	double elapsed;
 
 	printf("Measuring CPU processor ...\n");
 
-	t1 = rdtsc();
+	gettimeofday(&t1, (struct timezone *) 0);
+	c1 = rdtsc();
 
 	sleep(interval);
 
-	t2 = rdtsc();
+	c2 = rdtsc();
+	gettimeofday(&t2, (struct timezone *) 0);
 
-	freq = (double)(t2 - t1) / (double)interval;
+	//in microsecond
+	elapsed = (double)(t2.tv_sec - t1.tv_sec) * 1000000.00;
+
+	+ (double)(t2.tv_usec - t1.tv_usec);
+
+	if (t2.tv_usec < t1.tv_usec) {
+		t2.tv_usec += 1000000;
+		t2.tv_sec--;
+	}
+
+	elapsed += (double)(t2.tv_usec - t1.tv_usec);
+
+	elapsed = elapsed / 1000000.00;
+
+	freq = (double)(c2 - c1) / elapsed;
 
 	printf("Measured processor frequency: %.2f Hz (%.2f GHz)\n", freq, freq / 1000000000.00f);
 
