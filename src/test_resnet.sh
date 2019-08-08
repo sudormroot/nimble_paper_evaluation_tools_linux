d78ecefaf9fcf5c3c75124c43339ebffc4fa35ed
sudormroot <sudormroot@163.com>
2019-08-08 09:25:46 +0800

    changes on test_resnet.sh

--- src/test_resnet.sh
+++ src/test_resnet.sh
@@ -8,20 +8,29 @@
 
 threads_num="`cat /proc/cpuinfo|grep processor|wc -l`"
 
-thp_migration=1
+thp_migration=0
 
 max_mem_size="`cat /proc/meminfo |grep MemTotal|cut -d: -f2|cut -dk -f1`"
 max_mem_size="`expr $max_mem_size / 1024`"
 
 fast_mem_size=1024
 
-echo "Detected memory size: $max_mem_size"
+echo "Detected memory size: $max_mem_size MB"
 echo "Detected cpus       : $threads_num"
 
+python_program="~/resnet-in-tensorflow/cifar10_train.py"
 
-./launch_testee.sh      --thp_migration=1 \
+
+
+if [ ! -f "$python_program" ];then
+	echo "$python_program not found"
+	exit
+fi
+
+./launch_testee.sh      --thp_migration=$thp_migration \
                         --max-mem-size=$max_mem_size \
                         --fast-mem-size=$fast_mem_size \
                         --migration-threads-num=$threads_num \
-                        python ~/resnet-in-tensorflow/cifar10_train.py
+                        python $python_program
+
 
