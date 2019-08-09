8df093eba055745e090f5aa16b6533596755b162
sudormroot <sudormroot@163.com>
2019-08-09 12:43:34 +0800

    change extract_resnet_speed.sh

--- src/extract_resnet_speed.sh
+++ src/extract_resnet_speed.sh
@@ -6,11 +6,9 @@ if [ "$1" = "" ];then
         exit
 fi
 
-result_dir="$1"
 
-training_speed_list="`cat $result_dir/applog.txt |grep examples|awk '{print $8}'|sed 's/(//g'`"
+result_dirs_list="`echo result_dir-*`"
 
-fastmem_size="`cat $result_dir/config.txt |grep FAST_MEM_SIZE|cut -d= -f2`"
 
 min="0"
 max="0"
@@ -18,7 +16,13 @@ avg="0"
 sum="0"
 n="0"
 
-for x in $training_speed_list;do
+extract_data_one_dir(){
+	result_dir="$1"
+
+	training_speed_list="`cat $result_dir/applog.txt |grep examples|awk '{print $8}'|sed 's/(//g'`"
+	fastmem_size="`cat $result_dir/config.txt |grep FAST_MEM_SIZE|cut -d= -f2`"
+
+	for x in $training_speed_list;do
 
         echo "speed=$x"
 
@@ -33,9 +37,14 @@ for x in $training_speed_list;do
         sum="`echo $sum + $x | bc`"
 
         n="`expr $n + 1`"
+	done
+
+	avg="`echo $sum / $n | bc`"
+
+	echo "fastmem_size=$fastmem_size MB, training speed min=$min max=$max avg=$avg"
+}
+
+for dir in $result_dirs_list;do
+	echo "dir=$dir"
+	extract_data_one_dir $dir
 done
-
-avg="`echo $sum / $n | bc`"
-
-echo "fastmem_size=$fastmem_size MB, training speed min=$min max=$max avg=$avg"
-
