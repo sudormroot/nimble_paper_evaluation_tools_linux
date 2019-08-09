539486fc41092d6c38e37b08910cd4d2fb461754
sudormroot <sudormroot@163.com>
2019-08-09 20:15:00 +0800

    changes

--- src/test_numa_luanch.sh
+++ src/test_numa_luanch.sh
@@ -2,4 +2,11 @@
 
 sudo mkdir /sys/fs/cgroup/test 2>/dev/zero
 
-sudo ./numa_launch --cgroup=/sys/fs/cgroup/test --cpu-node=1 --fast-mem-size=256  --fast-mem-node=1 --slow-mem-node=0 python ~/resnet-in-tensorflow/cifar10_train.py
+memsize="$1"
+
+if [ "$memsize" = "" ];then
+	echo "$0 <fastmem-size-in-mb>"
+	exit
+fi
+
+sudo ./numa_launch --cgroup=/sys/fs/cgroup/test --cpu-node=1 --fast-mem-size=$memsize --fast-mem-node=1 --slow-mem-node=0 python ~/resnet-in-tensorflow/cifar10_train.py
