a02c7ffe15d55058168b08e48a8648319fa2be85
sudormroot <sudormroot@163.com>
2020-03-21 10:31:38 +0800

    add

--- src/test_warpx.sh
+++ src/test_warpx.sh
@@ -2,7 +2,6 @@
 
 #./launch_warpx.sh <mpi-ranks> <omp-threads> <fast-mem-size-in-mb> <num-of-migration-threads> <migration-interval-seconds> <warpx_cmd> <problem>
 
-./launch_warpx.sh 2 8 40 8 1 /home/jiaolin/warpx_pmdk_test/warpx_build/warpx_3d /home/jiaolin/warpx_pmdk_test/warpx_problems/laser-driven-acceleration/test_3d_64x64x512
 
 mkdir /home/jiaolin/warpx_pmdk_test/results_nimble
 chowrn -R jiaolin /home/jiaolin/warpx_pmdk_test/results_nimble
