94629f7ddd6691ed8f5b24fb0bbffcee0e428f62
sudormroot <sudormroot@163.com>
2020-03-21 16:07:10 +0800

    fix

--- src/numa_launch.c
+++ src/numa_launch.c
@@ -152,7 +152,7 @@ int main(int argc, char **argv)
     memset(slowmem_node, 0, sizeof(slowmem_node));
     
 	//while ((c = getopt_long(argc, argv, "s:F:S:C:c:", long_options, &option_index)) != -1) {
-	while ((c = getopt_long(argc, argv, "H:S:C:F:", long_options, &option_index)) != -1) {
+	while ((c = getopt_long(argc, argv, "C:S:F:", long_options, &option_index)) != -1) {
 
 		switch (c) {
 			//case 's':
