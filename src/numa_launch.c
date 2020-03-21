50f49b639812336bb401c14c8b24f449138d3409
sudormroot <sudormroot@163.com>
2020-03-21 15:22:50 +0800

    add

--- src/numa_launch.c
+++ src/numa_launch.c
@@ -183,7 +183,7 @@ int main(int argc, char **argv)
 	}
 
 
-	if(fastmem_node[0] == 0 || slowmem_node[0] == 0/* || cpu_node[0] == 0 */) {
+	if(fastmem_mask == NULL || slowmem_mask == NULL /* || cpu_node[0] == 0 */) {
 	//if(fastmem_node == -1 || cpu_node == -1) {
         printf("--fast-mem-node or --slow-mem-node must be set\n");
 		usage(argv[0]);
